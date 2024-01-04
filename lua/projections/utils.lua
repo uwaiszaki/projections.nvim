ae8feebe60ac3248ddef2603fe319f74e425e6ce
Uwais Zaki <uwaiszaki104@gmail.com>
2024-01-04 18:52:58 +0530

    Remove modified buffer check when changing session

--- lua/projections/utils.lua
+++ lua/projections/utils.lua
@@ -4,12 +4,12 @@ local M = {}
 ---@return boolean
 ---@nodiscard
 M._unsaved_buffers_present = function()
-    for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-        if vim.api.nvim_buf_is_loaded(buf) and vim.api.nvim_buf_get_option(buf, 'modified') then
-            return true
-        end
-    end
-    return false
+	-- for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+	--     if vim.api.nvim_buf_is_loaded(buf) and vim.api.nvim_buf_get_option(buf, 'modified') then
+	--         return true
+	--     end
+	-- end
+	return false
 end
 
 -- Calculate fnv1a hash
@@ -17,14 +17,14 @@ end
 ---@return integer
 ---@nodiscard
 M._fnv1a = function(s)
-    local bit = require("bit")
-    local prime = 1099511628211ULL
-    local hash = 14695981039346656037ULL
-    for i = 1, #s do
-        hash = bit.bxor(hash, s:byte(i))
-        hash = hash * prime
-    end
-    return tonumber(hash)
+	local bit = require("bit")
+	local prime = 1099511628211ULL
+	local hash = 14695981039346656037ULL
+	for i = 1, #s do
+		hash = bit.bxor(hash, s:byte(i))
+		hash = hash * prime
+	end
+	return tonumber(hash)
 end
 
 -- Returns unique workspaces in list
@@ -32,16 +32,16 @@ end
 ---@return Workspace[]
 ---@nodiscard
 M._unique_workspaces = function(workspaces)
-    local hashmap = {}
-    local result = {}
-    for _, ws in ipairs(workspaces) do
-        local hash = tostring(ws.path)
-        if not hashmap[hash] then
-            result[#result + 1] = ws
-            hashmap[hash] = true
-        end
-    end
-    return result
+	local hashmap = {}
+	local result = {}
+	for _, ws in ipairs(workspaces) do
+		local hash = tostring(ws.path)
+		if not hashmap[hash] then
+			result[#result + 1] = ws
+			hashmap[hash] = true
+		end
+	end
+	return result
 end
 
 return M
