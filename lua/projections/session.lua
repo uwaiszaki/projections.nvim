340592e50b01cc828828ec6df33765d26819b6fc
Uwais Zaki <uwaiszaki104@gmail.com>
2024-01-08 00:59:44 +0530

    Refactor

--- lua/projections/session.lua
+++ lua/projections/session.lua
@@ -20,6 +20,7 @@ function Session.info(spath)
 	local workspace_path = path:parent()
 	local all_workspaces = Workspace.get_workspaces()
 	local workspace = nil
+	vim.notify(vim.inspect(all_workspaces))
 	vim.notify("Workspace Path: " .. workspace_path)
 	vim.notify("Project Name: " .. project_name)
 
