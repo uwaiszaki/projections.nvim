1ba0a5f9c1baee72ff042b4581e18d7d0d1c7f50
Uwais Zaki <uwaiszaki104@gmail.com>
2024-01-07 20:26:42 +0530

    Add checks for opened non named buffers

--- lua/projections/utils.lua
+++ lua/projections/utils.lua
@@ -4,11 +4,15 @@ local M = {}
 ---@return boolean
 ---@nodiscard
 M._unsaved_buffers_present = function()
-	-- for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-	--     if vim.api.nvim_buf_is_loaded(buf) and vim.api.nvim_buf_get_option(buf, 'modified') then
-	--         return true
-	--     end
-	-- end
+	for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+		if
+			vim.api.nvim_buf_is_loaded(buf)
+			and vim.api.nvim_buf_get_option(buf, "modified")
+			and vim.api.nvim_buf_get_name(buf) ~= ""
+		then
+			return true
+		end
+	end
 	return false
 end
 
