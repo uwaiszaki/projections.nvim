b4b957df88a142aa8734945d6602a9edb5277480
Uwais Zaki <uwaiszaki104@gmail.com>
2024-01-07 23:00:35 +0530

    Add current active session sign in projections list

--- lua/telescope/_extensions/projections.lua
+++ lua/telescope/_extensions/projections.lua
@@ -7,76 +7,87 @@ local action_state = require("telescope.actions.state")
 local conf = require("telescope.config").values
 
 local function project_finder(opts)
-    local workspaces = require("projections.workspace").get_workspaces()
-    local projects = {}
-    for _, ws in ipairs(workspaces) do
-        for _, project in ipairs(ws:projects()) do
-            table.insert(projects, project)
-        end
-    end
+	local workspaces = require("projections.workspace").get_workspaces()
+	local projects = {}
+	local current_dir = vim.fn.getcwd()
 
-    return finders.new_table({
-        results = projects,
-        entry_maker = opts.entry_maker or function(project)
-            return {
-                display = function(e)
-                    local display = entry_display.create({
-                        items = { { width = 35 }, { remaining = true } },
-                        separator = " ",
-                    })
-                    return display({ e.name, { e.value, "Comment" } })
-                end,
-                name = project.name,
-                value = tostring(project:path()),
-                ordinal = tostring(project:path()),
-            }
-        end,
-    })
+	for _, ws in ipairs(workspaces) do
+		for _, project in ipairs(ws:projects()) do
+			table.insert(projects, project)
+		end
+	end
+
+	return finders.new_table({
+		results = projects,
+		entry_maker = opts.entry_maker or function(project)
+			return {
+				display = function(e)
+					local display = entry_display.create({
+						items = { { width = 35 }, { remaining = true } },
+						separator = " ",
+					})
+					local project_path = e.value
+					local project_name = e.name
+
+					-- Prepend the symbol if the project is the active session
+					if project_path == current_dir then
+						project_name = "-> " .. project_name
+					end
+					return display({ e.name, { e.value, "Comment" } })
+				end,
+				name = project.name,
+				value = tostring(project:path()),
+				ordinal = tostring(project:path()),
+			}
+		end,
+	})
 end
 
 local find_projects = function(opts)
-    opts = opts or {}
+	opts = opts or {}
 
-    -- Sort by recent implemented by
-    -- hooking into the default scoring function, of generic_sorter
-    -- we check if prompt is empty, if so, score by file modification time (inverted)
-    -- if prompt is not empty, use the default scoring function
-    local config = require("projections.config").config
-    local Session = require("projections.session")
-    local sorter = conf.generic_sorter(opts)
-    local default_scoring_function = sorter.scoring_function
-    sorter.scoring_function = function(_sorter, prompt, line, ...)
-        if prompt == '' then
-            local session_filename = Session.session_filename(vim.fn.fnamemodify(line, ":h"), vim.fs.basename(line))
-            return 1 / math.abs(vim.fn.getftime(tostring(config.sessions_directory .. session_filename)))
-        end
-        return default_scoring_function(_sorter, prompt, line, ...)
-    end
+	-- Sort by recent implemented by
+	-- hooking into the default scoring function, of generic_sorter
+	-- we check if prompt is empty, if so, score by file modification time (inverted)
+	-- if prompt is not empty, use the default scoring function
+	local config = require("projections.config").config
+	local Session = require("projections.session")
+	local sorter = conf.generic_sorter(opts)
+	local default_scoring_function = sorter.scoring_function
+	sorter.scoring_function = function(_sorter, prompt, line, ...)
+		if prompt == "" then
+			local session_filename = Session.session_filename(vim.fn.fnamemodify(line, ":h"), vim.fs.basename(line))
+			return 1 / math.abs(vim.fn.getftime(tostring(config.sessions_directory .. session_filename)))
+		end
+		return default_scoring_function(_sorter, prompt, line, ...)
+	end
 
-    local switcher = require("projections.switcher")
-    pickers.new(opts, {
-        prompt_title = "Projects",
-        finder = project_finder(opts),
-        sorter = sorter,
-        attach_mappings = function(prompt_bufnr)
-            actions.select_default:replace(function()
-                actions.close(prompt_bufnr)
-                local selection = action_state.get_selected_entry()
-                if opts.action == nil then
-                    opts.action = function(selected)
-                        if selected ~= nil and selected.value ~= vim.loop.cwd() then
-                            switcher.switch(selected.value)
-                        end
-                    end
-                end
-                opts.action(selection)
-            end)
-            return true
-        end,
-    }):find()
+	local switcher = require("projections.switcher")
+	pickers
+		.new(opts, {
+			prompt_title = "Projects",
+			finder = project_finder(opts),
+			sorter = sorter,
+			attach_mappings = function(prompt_bufnr)
+				actions.select_default:replace(function()
+					actions.close(prompt_bufnr)
+					local selection = action_state.get_selected_entry()
+					if opts.action == nil then
+						opts.action = function(selected)
+							if selected ~= nil and selected.value ~= vim.loop.cwd() then
+								switcher.switch(selected.value)
+							end
+						end
+					end
+					opts.action(selection)
+				end)
+				return true
+			end,
+		})
+		:find()
 end
 
 return telescope.register_extension({
-    setup = function(_, _) end,
-    exports = { projections = find_projects },
+	setup = function(_, _) end,
+	exports = { projections = find_projects },
 })
